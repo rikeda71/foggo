163701f5d8b02b1219aae198257b7eadc735dfd8
s14t284 <rikeda71@gmail.com>
2022-01-03 17:12:08 +0900

    :white_check_mark: add parser test

--- internal/parser/package_test.go
+++ internal/parser/package_test.go
@@ -1,15 +1,14 @@
-package parser_test
+package parser
 
 import (
 	"fmt"
 	"os"
 	"testing"
 
-	"github.com/s14t284/foggo/internal/parser"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParsePackageInfo1(t *testing.T) {
+func TestParsePackageInfo(t *testing.T) {
 	cwd, err := os.Getwd()
 	if err != nil {
 		t.FailNow()
@@ -21,16 +20,16 @@ func TestParsePackageInfo1(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    *parser.PackageInfo
+		want    *PackageInfo
 		wantErr assert.ErrorAssertionFunc
 	}{
-		{"nominal", args{cwd + "/../../testdata"}, &parser.PackageInfo{Name: "testdata"}, assert.NoError},
-		{"non_nominal: not found package", args{"./foo/bar"}, &parser.PackageInfo{Name: ""}, assert.NoError},
+		{"nominal", args{cwd + "/../../testdata"}, &PackageInfo{Name: "testdata"}, assert.NoError},
+		{"non_nominal: not found package", args{"./foo/bar"}, &PackageInfo{Name: ""}, assert.NoError},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			a := assert.New(t)
-			got, err := parser.ParsePackageInfo(tt.args.path)
+			got, err := ParsePackageInfo(tt.args.path)
 			if !tt.wantErr(t, err, fmt.Sprintf("ParsePackageInfo(%v)", tt.args.path)) {
 				return
 			}
