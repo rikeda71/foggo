2bf3fde333e5299e7584a29cebc6b71428b52523
s14t284 <rikeda71@gmail.com>
2022-01-03 14:31:11 +0900

    :sparkles: add functional option pattern generator

---
+++ internal/generator/generator.go
@@ -0,0 +1,107 @@
+package generator
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+	"text/template"
+)
+
+type Generator struct {
+	goimports bool
+}
+
+func InitializeGenerator() *Generator {
+	return &Generator{
+		goimports: true,
+	}
+}
+
+// GenerateFOP is the function to generate code of functional option pattern from struct
+func (g *Generator) GenerateFOP(pkgName string, structName string, sts []*StructField) (string, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("get executable directory error: %w", err)
+	}
+
+	tpl := template.Must(template.New("functional_option_pattern.go.tpl").ParseFiles(cwd + "/internal/generator/templates/functional_option_pattern.go.tpl"))
+	if !g.checkStructFieldFormat(sts) {
+		return "", fmt.Errorf("%s have same name fields", structName)
+	}
+
+	data := map[string]interface{}{
+		"pkgName":    pkgName,
+		"structName": structName,
+		"fields":     sts,
+	}
+	buf := &bytes.Buffer{}
+	if err := tpl.Execute(buf, data); err != nil {
+		return "", fmt.Errorf("render code with template error: %w", err)
+	}
+
+	return g.format(buf)
+}
+
+func (g *Generator) format(b *bytes.Buffer) (string, error) {
+	var err error
+	code := b.String()
+	if g.goimports {
+		code, err = g.applyGoimports(code)
+		if err != nil {
+			return "", err
+		}
+	}
+	return code, nil
+}
+
+func (g *Generator) applyGoimports(code string) (string, error) {
+	cmd := exec.Command("goimports")
+	stdinPipe, err := cmd.StdinPipe()
+	if err != nil {
+		return "", fmt.Errorf("get stdin pipeline error in execution goimports: %w", err)
+	}
+
+	out, errOut := &bytes.Buffer{}, &bytes.Buffer{}
+	cmd.Stdout = out
+	cmd.Stderr = errOut
+
+	err = cmd.Start()
+	if err != nil {
+		return "", fmt.Errorf("exec goimports error: %w", err)
+	}
+
+	_, err = io.WriteString(stdinPipe, code)
+	if err != nil {
+		return "", fmt.Errorf("write goimports result error: [error][%w][errOut][%s]", err, errOut.String())
+	}
+	err = stdinPipe.Close()
+	if err != nil {
+		return "", fmt.Errorf("close stdin pipeline error in execution goimports: %w", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return "", fmt.Errorf("write goimports result error: [error][%w][errOut][%s]", err, errOut.String())
+	}
+
+	return out.String(), nil
+}
+
+func (g *Generator) checkStructFieldFormat(sts []*StructField) bool {
+	for _, st1 := range sts {
+		name := strings.ToLower(st1.Name)
+		cnt := 0
+		for _, st2 := range sts {
+			if name == st2.Name {
+				cnt++
+			}
+		}
+		if cnt > 1 {
+			return false
+		}
+	}
+	return true
+}
