fc98c3a3c66dd65598f6d63f73767efb8bcd9b85
s14t284 <rikeda71@gmail.com>
2022-01-09 14:05:52 +0900

    :white_check_mark: add foc command test

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -10,7 +10,7 @@ func Test_initializeRootCmd(t *testing.T) {
 	// assert initialization and check required parameters
 	a := assert.New(t)
 	cmd, err := initializeRootCmd()
-	a.NotNil(err)
+	a.NoError(err)
 	a.NotEqual("", cmd.Use)
 	a.NotEqual("", cmd.Short)
 	a.NotEqual("", cmd.Long)
