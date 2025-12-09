3e510737646bccf661fbef3108b3a0d2b930e543
Ryuya Ikeda <rikeda71@gmail.com>
2025-12-09 22:56:19 +0900

    fix: resolve golangci-lint staticcheck warnings
    - Replace strings.Replace with strings.ReplaceAll in fields.go
    - Use Printf instead of Println(Sprintf(...)) in writer.go

--- internal/parser/fields.go
+++ internal/parser/fields.go
@@ -91,7 +91,7 @@ func parseTag(tag *ast.BasicLit) (*structtag.Tag, error) {
 		return nil, nil
 	}
 
-	tags, err := structtag.Parse(strings.Replace(tag.Value, "`", "", -1))
+	tags, err := structtag.Parse(strings.ReplaceAll(tag.Value, "`", ""))
 	if err != nil {
 		return nil, fmt.Errorf("parse tag error: %w", err)
 	}
